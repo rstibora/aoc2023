72a95c2920fa768d4ee338e71f9ef001a84f82da
Radek Stibora <rstibora@gmail.com>
2024-01-03 07:29:29 +0100

    Add second star solution for day 1

--- day01.exs
+++ day01.exs
@@ -5,7 +5,28 @@ defmodule Day01 do
                  |> Stream.map(fn line -> String.to_integer(line) end)
                  |> Enum.reduce(0, fn a, b -> a + b end)
   end
+
+  defp replace_numbers(line) do
+    line = String.replace(line, "one", "one1one")
+    line = String.replace(line, "two", "two2two")
+    line = String.replace(line, "three", "three3three")
+    line = String.replace(line, "four", "four4four")
+    line = String.replace(line, "five", "five5five")
+    line = String.replace(line, "six", "six6six")
+    line = String.replace(line, "seven", "seven7seven")
+    line = String.replace(line, "eight", "eight8eight")
+    line = String.replace(line, "nine", "nine9nine")
+    line
+  end
+
+  def second_star(input_stream) do
+    input_stream |> Stream.map(fn line -> replace_numbers(line) end)
+                 |> first_star
+  end
 end
 
+
 input_stream = File.stream!("inputs/day01.txt")
-IO.puts(Day01.first_star(input_stream))
+IO.puts("First star: '#{Day01.first_star(input_stream)}'")
+input_stream = File.stream!("inputs/day01.txt")
+IO.puts("Second star: '#{Day01.second_star(input_stream)}'")
